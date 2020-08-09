620cd81fc702b1fa532d4957d3372c3c81891c6a
Yerassyl Zhylkybayev <erasiloco@gmail.com>
2020-08-09 22:17:28 +0600

    drop down menu

---
+++ Akron/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akron.Models
+{
+    public class User
+    {
+        public User(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+        public bool CanDownload { get; set; }
+        public bool CanDelete { get; set; }
+        public bool CanModify { get; set; }
+        public bool CanCreate { get; set; }
+        public bool CanRead { get; set; }
+    }
+}
---
+++ Akron/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿var checkboxes = $('.user-checkbox');
+var total = checkboxes.length;
+var checkall = $('#checkAll');
+
+checkall.change(function () {
+    checkboxes.prop('checked', $(this).is(':checked'));
+})
+
+checkboxes.change(function () {
+    var count = checkboxes.filter(':checked').length;
+    checkall.prop('checked', (count == total));
+})
+var menuOpened = false;
+function menuClick() {
+    if (menuOpened) {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("content").style.marginLeft = "0";
+        menuOpened = false;
+    }
+    else {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("content").style.marginLeft = "250px";
+        menuOpened = true;
+    }    
+}
+
+function dropSubMenu() {
+    document.getElementById("adminMenu").classList.toggle("show");
+}
